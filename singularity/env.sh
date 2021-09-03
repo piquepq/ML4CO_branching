c8e3d8edb616e244e2e4c63e178aafe91e6b8013
LoganZhao1997 <wentao.zhaowt@gmail.com>
2021-09-03 19:30:52 -0400

    change cuda version

--- singularity/env.sh
+++ singularity/env.sh
@@ -9,7 +9,8 @@ conda env create -n ml4co -f conda.yaml
 conda activate ml4co
 
 # install pytorch with cuda 10.2
-conda install -y pytorch==1.9.0 torchvision torchaudio cudatoolkit=10.2 -c pytorch
+# conda install -y pytorch==1.9.0 torchvision torchaudio cudatoolkit=10.2 -c pytorch
+conda install -y pytorch==1.9.0 torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
 
 # install pytorch geometric
 conda install -y pytorch-geometric -c rusty1s -c conda-forge
