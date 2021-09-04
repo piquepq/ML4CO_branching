a6c0aba82764a6d5ccbd81d1ba475e644a9ecf56
LoganZhao1997 <wentao.zhaowt@gmail.com>
2021-09-03 21:58:34 -0400

    check singularity bind

--- singularity/02generate_data.sh
+++ singularity/02generate_data.sh
@@ -16,11 +16,14 @@ fi
 
 export SINGULARITY_HOME=`realpath $PWD`
 export SINGULARITY_BIND="$(mktemp -d):/tmp,$(mktemp -d):/var/tmp"
-export SINGULARITY_BIND="${SINGULARITY_BIND},/rigel/seasdean/projects/ml4co/instances:$SINGULARITY_HOME/instances:ro"
 export SINGULARITY_CLEANENV=1
 export SINGULARITY_CONTAINALL=1
 export SINGULARITY_NV=1
 export SINGULARITY_NETWORK=none
 
+# bind
+singularity exec --bind /rigel/seasdean/projects/ml4co/instances:/instances singularity/base.sif
+
+# generate samples
 COMMANDS="source /opt/mamba/init.bash; conda activate ml4co; python bc/01_generate_dataset.py $1"
 singularity exec --net singularity/base.sif bash -i -c "$COMMANDS"
--- singularity/04evolutionary_strategy.sh
+++ singularity/04evolutionary_strategy.sh
@@ -13,11 +13,14 @@ fi
 
 export SINGULARITY_HOME=`realpath $PWD`
 export SINGULARITY_BIND="$(mktemp -d):/tmp,$(mktemp -d):/var/tmp"
-export SINGULARITY_BIND="${SINGULARITY_BIND},/rigel/seasdean/projects/ml4co/instances:$SINGULARITY_HOME/instances:ro"
 export SINGULARITY_CLEANENV=1
 export SINGULARITY_CONTAINALL=1
 export SINGULARITY_NV=1
 export SINGULARITY_NETWORK=none
 
+# bind
+singularity exec --bind /rigel/seasdean/projects/ml4co/instances:/instances singularity/base.sif
+
+# es
 COMMANDS="source /opt/mamba/init.bash; conda activate ml4co; python es/src/main.py $@"
 singularity exec --net singularity/base.sif bash -i -c "$COMMANDS"
